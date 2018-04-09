1038f17d7ac56cf9708f7e074a6a77033e55c12a
SpectralJager <unkn.spectrum@gmail.com>
2018-04-09 20:01:26 +0300

    "about"Done

--- adoutwind.py
+++ adoutwind.py
@@ -1,9 +1,12 @@
 import sys
 from PyQt5.QtWidgets import QWidget
 from PyQt5 import uic
+from PyQt5.QtCore import QUrl
 
+SOURCE = 'about.html'
 
 class Aboutwind(QWidget):
 	def __init__(self):
 		super().__init__()
 		uic.loadUi('aboutwind.ui',self)
+		self.textBrowser.setSource(QUrl(SOURCE))
